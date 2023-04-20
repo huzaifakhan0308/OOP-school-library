26f2400021e90649fb8e16a50ea6c912f1747b93
Ermi <manermidem@gmail.com>
2023-04-20 12:13:09 +0300

    Add a test for the add_rentals method in book.rb.

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,4 +1,6 @@
 require_relative "../book.rb"
+require_relative "../person.rb"
+require_relative "../rental.rb"
 
 describe Book do
     before(:each) do
@@ -16,6 +18,22 @@ describe Book do
     
         it "returns the correct author" do
         expect(@book.author).to eq("Paulo Coelho")
+        end
+    end
+
+    describe "#add_rentals" do
+        context "when the book is rented" do
+        
+            it "checks if the book has a rental" do
+                expect(@book.rentals).to be_empty
+            end
+
+            it "checks if the book has a rental" do
+                @person = Person.new(26, "Ermiyas", parent_permission: true)
+                @rental = Rental.new("2021-01-01", @book, @person)        
+               @book.add_rentals(@person, "2021-01-01")
+                expect(@book.rentals).to include(@rental)
+            end
         end
     end
 end
