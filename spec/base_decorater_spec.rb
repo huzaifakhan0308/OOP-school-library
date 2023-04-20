21a21d31cbdac93280f1672e5384417df370a890
Ermi <manermidem@gmail.com>
2023-04-20 15:12:05 +0300

    Fix linters

--- spec/base_decorater_spec.rb
+++ spec/base_decorater_spec.rb
@@ -1,27 +1,25 @@
-require_relative "../base_decorater"
+require_relative '../base_decorater'
 
 describe CapitalizeDecorator do
+  before(:each) do
+    @capitalize_decorator = CapitalizeDecorator.new('hello')
+  end
 
-    before(:each) do
-        @capitalize_decorator = CapitalizeDecorator.new("hello")
+  describe '#new' do
+    it 'returns a capitalize decorator object' do
+      expect(@capitalize_decorator).to be_an_instance_of(CapitalizeDecorator)
     end
-    
-    describe '#new' do
-        it 'returns a capitalize decorator object' do
-        expect(@capitalize_decorator).to be_an_instance_of(CapitalizeDecorator)
-        end
-    end
+  end
 end
 
 describe TrimmerDecorator do
+  before(:each) do
+    @trimmer_decorator = TrimmerDecorator.new('hello')
+  end
 
-    before(:each) do
-        @trimmer_decorator = TrimmerDecorator.new("hello")
+  describe '#new' do
+    it 'returns a trimmer decorator object' do
+      expect(@trimmer_decorator).to be_an_instance_of(TrimmerDecorator)
     end
-    
-    describe '#new' do
-        it 'returns a trimmer decorator object' do
-        expect(@trimmer_decorator).to be_an_instance_of(TrimmerDecorator)
-        end
-    end
+  end
 end
