c2b4c103a903b2f0453eaa841f1303a555da56c9
Ermi <manermidem@gmail.com>
2023-04-20 13:12:45 +0300

    Fix linters error

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,30 +1,29 @@
-require_relative "../student.rb"
+require_relative '../student'
 
 describe Student do
+  before(:each) do
+    @student = Student.new(26, 'Maths', 'Ermiyas', parent_permission: true)
+  end
 
-    before(:each) do
-        @student = Student.new(26, "Maths", "Ermiyas", parent_permission: true)
+  describe '#new' do
+    it 'returns a student object' do
+      expect(@student).to be_an_instance_of(Student)
     end
+  end
 
-    describe "#new" do
-        it "returns a student object" do
-            expect(@student).to be_an_instance_of(Student)
-        end
+  describe '#play_hooky' do
+    context 'when the student' do
+      it 'checks if the student is playing hooky' do
+        expect(@student.play_hooky).to eq('¯\\(ツ)/¯')
+      end
     end
+  end
 
-    describe "#play_hooky" do
-        context "when the student" do
-            it "checks if the student is playing hooky" do
-                expect(@student.play_hooky).to eq('¯\\(ツ)/¯')
-            end
-        end
+  describe '#classrooms' do
+    context 'when the student' do
+      it 'checks if the student has a classroom' do
+        expect(@student.classroom).to eq('Maths')
+      end
     end
-
-    describe "#classrooms" do
-        context "when the student" do
-            it "checks if the student has a classroom" do
-                expect(@student.classroom).to eq("Maths")
-            end
-        end
-    end
+  end
 end
