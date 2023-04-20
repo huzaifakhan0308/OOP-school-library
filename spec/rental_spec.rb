e145dd974a2ff0f619a2bb7b8bbdae9a0cd27bc0
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-04-20 16:01:09 +0500

    write tests for rentals.rb file

---
+++ spec/rental_spec.rb
@@ -0,0 +1,30 @@
+require_relative "../rental"
+require_relative "../person"
+require_relative "../book"
+
+describe Rental do
+    before(:each) do
+        @person = Person.new(26, "Ermiyas", parent_permission: true)
+        @book = Book.new("The Alchemist", "Paulo Coelho")
+        @rental = Rental.new("2021-01-01", @book, @person)
+    end
+    
+    describe "#new" do
+        it "returns a rental object" do
+        expect(@rental).to be_an_instance_of(Rental)
+        end
+    
+        it "returns the correct date" do
+        expect(@rental.date).to eq("2021-01-01")
+        end
+    
+        it "returns the correct book" do
+        expect(@rental.book).to eq(@book)
+        end
+    
+        it "returns the correct person" do
+        expect(@rental.person).to eq(@person)
+        end
+    end
+
+end
