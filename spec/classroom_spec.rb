08f6b8082f138c49c082c92593f679f44a8f4fbc
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-04-20 16:18:49 +0500

    fix linters

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,26 +1,24 @@
-require_relative "../classroom"
-require_relative "../student"
+require_relative '../classroom'
+require_relative '../student'
 
 describe Classroom do
+  before(:each) do
+    @classroom = Classroom.new('Maths')
+    @student = Student.new(26, 'Maths', 'Ermiyas', parent_permission: true)
+  end
 
-    before(:each) do
-        @classroom = Classroom.new("Maths")
-        @student = Student.new(26, "Maths", "Ermiyas", parent_permission: true)
+  describe '#new' do
+    it 'returns a classroom object' do
+      expect(@classroom).to be_an_instance_of(Classroom)
     end
-    
-    describe "#new" do
-        it "returns a classroom object" do
-        expect(@classroom).to be_an_instance_of(Classroom)
-        end
+  end
+
+  describe '#add_students' do
+    context 'when the student' do
+      it 'checks if the student has a classroom' do
+        @classroom.add_students(@student)
+        expect(@student.classroom).to eq(@classroom)
+      end
     end
-    
-    describe "#add_students" do
-        context "when the student" do
-        it "checks if the student has a classroom" do
-            @classroom.add_students(@student)
-            expect(@student.classroom).to eq(@classroom)
-        end
-        end
-    end
-    
+  end
 end
