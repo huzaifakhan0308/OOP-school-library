b14cb49de337d4136935ad35a8528e9f2bc5dbc2
Ermi <manermidem@gmail.com>
2023-04-20 12:37:21 +0300

    Add test for instance of person for person.rb

---
+++ spec/person_spec.rb
@@ -0,0 +1,20 @@
+require_relative "../person.rb"
+require_relative "../rental.rb"
+require_relative "../book.rb"
+
+describe Person do
+  
+    before(:each) do
+        @person = Person.new(26, "Ermiyas", parent_permission: true)
+        @book = Book.new("The Alchemist", "Paulo Coelho")
+    end
+
+    describe "#new" do
+    
+        context "when the person" do
+            it "returns a person object" do
+                expect(@person).to be_an_instance_of(Person)
+            end
+        end
+    end
+end
